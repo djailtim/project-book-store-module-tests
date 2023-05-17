bd9dcab2cdf795f9b1c9d0d46ab1008ec8bbe2de
Ailton Ferreira Luz <djailtim@gmail.com>
2023-05-17 15:02:03 -0300

    :tada: chore: First commit!

---
+++ src/main/java/com/ailton/projectbookstoremodulefinal/ProjectBookStoreModuleFinalApplication.java
@@ -0,0 +1,13 @@
+package com.ailton.projectbookstoremodulefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBookStoreModuleFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBookStoreModuleFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ailton/projectbookstoremodulefinal/ProjectBookStoreModuleFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailton.projectbookstoremodulefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBookStoreModuleFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
